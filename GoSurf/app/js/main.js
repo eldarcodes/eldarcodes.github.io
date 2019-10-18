ff18060649315783dc7df8c2aa3dc382cfd27f2b
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-10-18 21:06:14 +0300

    init

---
+++ GoSurf/app/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
+        asNavFor: '.slider-dots',
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
+});
