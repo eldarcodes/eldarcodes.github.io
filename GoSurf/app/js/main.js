4efd77b7ff1d1b5156899b8b608ef68159e1aa83
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-10-19 16:03:56 +0300

    gosurf update

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -5,12 +5,28 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
     });
 });
