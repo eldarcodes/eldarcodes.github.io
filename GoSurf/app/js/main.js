5d2377e724ad139d721532197ad549be83cf856f
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-12-14 15:52:08 +0200

    update

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -30,7 +30,7 @@ $(function () {
         focusOnSelect: true
     });
 
-    $('.holder__slider').slick({
+    $('.holder__slider, .shop__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
@@ -79,4 +79,8 @@ $(function () {
     let summ = $('.nights').val() * $('.summ').data('nights') 
     + ($('.guests').val() -1) * $('.summ').data('guests');
     $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click',function(){
+      $(this).toggleClass('active')
+    });
 });
