bd44db330f8d906c8494af25f7709bbb36151664
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-12-14 14:09:44 +0200

    all

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -29,4 +29,54 @@ $(function () {
         asNavFor: '.surf-slider',
         focusOnSelect: true
     });
+
+    $('.holder__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="">',
+    });
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg"></div><div class="quantity-button quantity-down"><img src="img/minus.svg"></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+    
+    $('.quantity-button').on('click', function(){
+      let summ = $('.nights').val() * $('.summ').data('nights') 
+    + ($('.guests').val() -1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
+    });
+
+    let summ = $('.nights').val() * $('.summ').data('nights') 
+    + ($('.guests').val() -1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
 });
