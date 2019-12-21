ebf93624812d13787a5fb3459cb573fd35701bcf
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-12-21 17:27:14 +0200

    added adaptive, animation

--- GoSurf/gulpfile.js
+++ GoSurf/gulpfile.js
@@ -46,7 +46,8 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/wow.js/dist/wow.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
