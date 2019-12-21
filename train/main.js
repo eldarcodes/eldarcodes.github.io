d3f0f61d56f28c2471eb45380d146056252687cc
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-12-21 17:30:55 +0200

    deleted past projects

--- train/main.js
+++
@@ -1,9 +0,0 @@
-let user = {
-    name: "John",
-    age: 30
-  };
-  
-  let key = prompt("Что вы хотите узнать о пользователе?", "name");
-  
-  // доступ к свойству через переменную
-  alert( user[key] ); // John (если ввели "name")
--- weather/main.js
+++
@@ -1,8 +0,0 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk')
-    .then(function(resp) {return resp.json()})
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function() {
-        
-    })
