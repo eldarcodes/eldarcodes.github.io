9fcf95931e9ce03466dbc0d97d9b744bce96b89f
Eldar Mirzabekov <prog.eldar@gmail.com>
2019-12-26 23:37:39 +0200

    added header

---
+++ miushi/app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('.main__slider').slick({
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/main-page/arrow-left.png" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/main-page/arrow-right.png" alt="">',
+    })
+
+
+});
