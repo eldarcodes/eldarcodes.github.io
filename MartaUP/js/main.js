02d9c89e7b968ba37e9fe60c5df72eac8d263e32
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-01-14 15:23:52 +0200

    added new project

---
+++ MartaUP/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        verticalSwiping: true,
+        dotsClass: 'header-dots',
+        // autoplay: 2000,
+    })
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    })
+});
