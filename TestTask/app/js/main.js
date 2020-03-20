64b9ee1be1c4c11f00665baf47e6928cf9360daa
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-03-20 16:16:44 +0200

    (TestTask) Сделано бургер-меню, поправлен слайдер, добавлена гугл-карта

--- TestTask/app/js/main.js
+++ TestTask/app/js/main.js
@@ -1,6 +1,11 @@
 $('.slider-wrapper').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  arrows: false,
+  dots: true
+  // autoplay: true,
+  // autoplaySpeed: 2000,
 })
+$('.header__burger').on('click', function() {
+  $('.header__burger, .header__inner-nav').toggleClass('active')
+  $('body').toggleClass('lock')
+})
+
