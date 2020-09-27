4abdec8e5f92e7a94dfd36b3c2bd3cf27cd54665
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-09-27 14:06:47 +0300

    Изменения cesar cipher

--- cesar/script.js
+++ cesar/script.js
@@ -1,24 +1,63 @@
-document.getElementById('in').oninput = function () {
-    const offset = 3;
-    let str = this.value;
+let key = document.getElementById('key')
 
-    let out = '';
-    for (let i = 0; i < str.length; i++) {
-        let code = str.charCodeAt(i);
-        code = code + offset;
-        out += String.fromCharCode(code);
-    }
-    document.getElementById('out').innerHTML = "Result: " + out;
+const cipher = document.getElementById('in')
+const decipher = document.getElementById('out2')
+
+key.addEventListener('input', () => {
+  cipherFunc()
+  decipherFunc()
+})
+
+cipher.addEventListener('input', () => {
+  cipherFunc()
+})
+
+decipher.addEventListener('input', () => {
+  decipherFunc()
+})
+
+const cipherFunc = () => {
+  let offset
+
+  if (key.value.length == 0) {
+    offset = 2
+  } else if (key.value.length >= 15) {
+    offset = 4
+  } else {
+    offset = key.value.length
+  }
+
+  let str = cipher.value
+  let out = ''
+
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i)
+    code += offset
+    out += String.fromCharCode(code)
+  }
+
+  document.getElementById('out').innerHTML = 'Result: ' + out
 }
 
-document.getElementById('out2').oninput = function () {
-    const offset = 3;
-    let str = this.value;
-    let out = '';
-    for (let i = 0; i < str.length; i++) {
-        let code = str.charCodeAt(i);
-        code = code - offset;
-        out += String.fromCharCode(code);
-    }
-    document.getElementById('out3').innerHTML = "Result: " + out;
-} 
+const decipherFunc = () => {
+  let offset
+
+  if (key.value.length == 0) {
+    offset = 2
+  } else if (key.value.length >= 7) {
+    offset = 3
+  } else {
+    offset = key.value.length
+  }
+
+  let str = decipher.value
+  let out = ''
+
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i)
+    code -= offset
+    out += String.fromCharCode(code)
+  }
+
+  document.getElementById('out3').innerHTML = 'Result: ' + out
+}
