f13dbe865cd67a944542709a7c1778a68f233d02
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-10-18 20:10:45 +0300

    debug

--- justmens/app/js/main.js
+++ justmens/app/js/main.js
@@ -20,9 +20,6 @@ $('.items').slick({
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 })
 
