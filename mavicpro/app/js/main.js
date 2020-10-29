612dddec4e60c77df8e514d6343406a9edefd5a1
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-10-29 22:35:47 +0200

    Завершена основная верстка

--- mavicpro/app/js/main.js
+++ mavicpro/app/js/main.js
@@ -1,9 +1,16 @@
 $(function () {
+  const activeClass = 'questions__item--active'
+
   $('.products__slider').slick({
     prevArrow: `<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M9.21839 1L1 9L9.21839 17" /></svg></button>`,
     nextArrow: `<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0.78161 17L9 9L0.78161 1" /></svg></button>`,
     infinite: false,
+  })
+
+  $('.questions__item-title').on('click', function () {
+    $('.questions__item').removeClass(activeClass)
+    $(this).parent().addClass(activeClass)
   })
 })
